bea1289d2224ea4cfe1303dfefb3a7b9b2f6fbc4
Tachaeus <tachincursion@gmail.com>
2025-09-21 14:21:53 +0300

    Update config.js

--- S_Server/utils/config.js
+++ S_Server/utils/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 const PORT = process.env.PORT || 5500;
-const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://Tachaeus:Scriptor1@cluster0.0lmovcn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const MONGO_URI = process.env.MONGO_URI
 
 module.exports = {
     PORT,
